da2efcae6bd7162db57f825358704cedd933c7b0
leee <your_email@whatever.com>
2018-09-11 10:07:45 +0800

    Initial Commit

---
+++ DesignPatternDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DesignPatternDemo
+//
+//  Created by Vinci on 2018/9/11.
+//  Copyright © 2018年 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
